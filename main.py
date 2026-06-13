bed10dca95890ad3f27d18b3325dd225a891a068
Sokolov Vladimir <vladimirsokolov10007@gmail.com>
2026-06-13 16:19:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 # --- НАСТРОЙКИ ---
 BOT_TOKEN = '8925355466:AAFwGby1v-t-JGbCbUnjbt_2aM9JTsnHU_U'
-CHAT_ID = '-1001234567890'
+CHAT_ID = '-1004445860179'
 
 # --- ТОЛЬКО СОРЕВНОВАТЕЛЬНЫЕ КЛЮЧЕВЫЕ СЛОВА ---
 # Событие должно содержать хотя бы одно из них
